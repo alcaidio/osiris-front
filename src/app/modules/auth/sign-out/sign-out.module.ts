dbd811e13492c69a37c9e3879ec3367b08ad152d
Timothy Alcaide <talcaide@immergis.fr>
2020-07-17 14:41:25 +0200

    feat(page/sign-out): add i18n

--- src/app/modules/auth/sign-out/sign-out.module.ts
+++ src/app/modules/auth/sign-out/sign-out.module.ts
@@ -5,9 +5,11 @@ import { TreoCardModule } from '@treo/components/card'
 import { SharedModule } from 'app/shared/shared.module'
 import { AuthSignOutComponent } from 'app/modules/auth/sign-out/sign-out.component'
 import { authSignOutRoutes } from 'app/modules/auth/sign-out/sign-out.routing'
+import { TRANSLOCO_SCOPE } from '@ngneat/transloco'
 
 @NgModule({
   declarations: [AuthSignOutComponent],
   imports: [RouterModule.forChild(authSignOutRoutes), MatButtonModule, TreoCardModule, SharedModule],
+  providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'sign-out' }],
 })
 export class AuthSignOutModule {}
