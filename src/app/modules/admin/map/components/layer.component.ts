279f19db3f1fd101e685b3ce129ea134887408d7
Timothy Alcaide <talcaide@immergis.fr>
2020-08-11 12:04:36 +0200

    fix(map/layer): fix toggle filter layer

--- src/app/modules/admin/map/components/layer.component.ts
+++ src/app/modules/admin/map/components/layer.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { Visibility } from 'mapbox-gl'
 import { Layer } from '../models/layer.model'
 
 @Component({
@@ -6,7 +7,12 @@ import { Layer } from '../models/layer.model'
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <ng-container *ngFor="let layer of layers; trackBy: trackByFn">
-      <mgl-layer *ngIf="layer.visible" [id]="layer.id" [type]="layer.type" [source]="layer.source"></mgl-layer>
+      <mgl-layer
+        [id]="layer.id"
+        [type]="layer.type"
+        [source]="layer.source"
+        [layout]="{ visibility: booleanToVisibility(layer.visible) }"
+      ></mgl-layer>
     </ng-container>
   `,
 })
@@ -15,5 +21,9 @@ export class LayerComponent {
 
   trackByFn(index: number, item: any): string {
     return item.id || index
+  }
+
+  booleanToVisibility(visible: boolean): Visibility {
+    return visible ? 'visible' : 'none'
   }
 }
