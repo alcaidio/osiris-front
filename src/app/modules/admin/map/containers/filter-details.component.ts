2892ea0137d8dc3236236e844f3590b9b267624c
Timothy Alcaide <talcaide@immergis.fr>
2020-08-26 11:09:51 +0200

    fix(map/filter): add no filters available template

--- src/app/modules/admin/map/containers/filter-details.component.ts
+++ src/app/modules/admin/map/containers/filter-details.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Select, Store } from '@ngxs/store'
 import { Observable } from 'rxjs'
-import { LayersState, OpenDrawer, ToggleLayer } from '../store'
-import { CloseDrawer } from './../store/actions/ui.action'
+import { LayersState, ToggleLayer } from '../store'
+import { CloseDrawer, OpenDrawer } from './../store/actions/ui.action'
 
 @Component({
   selector: 'app-filter-details',
   template: `
-    <ng-container *ngIf="filters$ | async as filters">
-      <div class="bg-gray-50 p-5 border-b border-gray-200">
-        <div class="text-lg leading-6 font-medium text-gray-900">
-          Filtres
-        </div>
+    <div class="bg-gray-50 p-5 border-b border-gray-200">
+      <div class="text-lg leading-6 font-medium text-gray-900">
+        Filtres
       </div>
-      <div class="px-4 py-3 mt-2">
+    </div>
+    <div class="px-4 py-3 mt-2">
+      <ng-container *ngIf="(filters$ | async)?.length > 0; else noFilters">
         <dt class="text-sm leading-5 font-medium text-gray-500 mb-3">
           Etat de la voirie
         </dt>
-        <dd class="ml-2 mt-1 text-sm leading-5 text-gray-900" *ngFor="let filter of filters">
+        <dd class="ml-2 mt-1 text-sm leading-5 text-gray-900" *ngFor="let filter of filters$ | async">
           <mat-checkbox color="primary" [checked]="filter.visible" (change)="onToggle(filter.id)">
             {{ filter.name }}
           </mat-checkbox>
         </dd>
-      </div>
-    </ng-container>
+      </ng-container>
+      <ng-template #noFilters>
+        Pas de filtres disponibles
+      </ng-template>
+    </div>
   `,
-  styles: [],
 })
 export class FilterDetailsComponent implements OnInit, OnDestroy {
   @Select(LayersState.getFilter) filters$: Observable<any>
