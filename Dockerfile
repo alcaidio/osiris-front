c2d64aef2ce28b7fed8439ec20b2d2f6a4eff566
Timothy Alcaide <talcaide@immergis.fr>
2020-10-07 15:10:43 +0200

    ci(gitlab): update project name and add env prod and modify gitlab ci

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM nginx:latest
-COPY dist/roadview/ /usr/share/nginx/html
+COPY dist/osiris/ /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/nginx.conf
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+// TODO FIX : change variable for production !!!!!!!!!
 export const environment = {
   production: true,
+  mapbox: {
+    api: {
+      token: 'pk.eyJ1IjoidGltb3RoeTM0IiwiYSI6ImNrYjZreXRmNzB6NXAyeW16ZDFyYzFhdmcifQ.PP2qxB_gEEAh9XzZMEQm0g',
+    },
+  },
+  osiris: {
+    api: 'http://192.168.0.187:10002/api',
+  },
 }
